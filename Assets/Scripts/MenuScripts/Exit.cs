33e241bee4b466e2e9616d65c77319bc4ce4d274
KirillMoskvin <kirill.moskvin@mail.ru>
2016-11-23 02:05:24 +0300

    Added menu navigation

---
+++ Assets/Scripts/MenuScripts/Exit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ExitClick()
+    {
+        Application.Quit();
+    }
+}
